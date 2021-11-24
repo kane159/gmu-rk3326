20ffdbe4a64cf0d9f08432de1e03151421af72c2
kane159 <kane12369@gmail.com>
2021-11-24 13:29:32 +0800

    Update pack.sh

--- pack.sh
+++ pack.sh
@@ -4,7 +4,7 @@ cp readme.install.txt ./dist/readme.txt
 cp gamelist.xml ./dist/gmu/
 cp -r gmu.bin rk3326.keymap gmuinput.rk3326.conf themes frontends decoders ./dist/gmu/
 cp gmu.rk3326.conf ./dist/gmu/gmu.conf
-cp gmuinput.rk3326.conf ./dist/gmu/gmuinput.rg351v.conf
+cp gmuinput.rk3326.conf ./dist/gmu/gmuinput.rk3326.conf
 cp rk3326.keymap ./dist/gmu/default.keymap
 cd ./dist/
 tar -czvf ../gmu-rk3326-bin.tar.gz ./
