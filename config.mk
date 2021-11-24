25b8171f0795cd3df270ea645031d370c62bcb42
kane159 <kane12369@gmail.com>
2021-11-24 12:53:58 +0800

    Update config.mk

--- config.mk
+++ config.mk
@@ -1,6 +1,6 @@
 
-CFLAGS=`/usr/bin/sdl2-config --cflags` \
-LFLAGS=`/usr/bin/sdl2-config --libs` \
-CC=/gcc ./configure \
+CFLAGS=`sdl2-config --cflags` \
+LFLAGS=`sdl2-config --libs` \
+CC=gcc ./configure \
 --sdk-path=/ \
 --target-device=rk3326
